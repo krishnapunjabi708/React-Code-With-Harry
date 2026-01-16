69cb565514b8a94cd5541445447bc05888184bf6
krishnapunjabi708 <krishnapunjabi708@gmail.com>
2026-01-16 17:44:00 +0530

    11 Video Complete Dark Mode and some extra function in website texutils

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import TextForm from './components/TextForm';
 import Navbar from './components/Navbar';
+// import About from './components/About';
 function App() {
   return (
     <>
 <Navbar title="TextUtils" about="About us"/>
 <div className="container"><TextForm heading="Enter text to analyze below"/></div>
-
+{/* <About /> */}
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,18 +8,55 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
+    const handleLoClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
     const handleOnChange=(event)=>{
         // console.log("On change");
         setText(event.target.value);
     }
+    const handleClearClick=()=>{     
+        let newText = '';
+        setText(newText);
+    }
+    const handleColorChange=()=>{   
+        let newText = text;
+        document.getElementById("myBox").style.color="blue";
+        setText(newText);
+    }
+    const handleCopy=()=>{   
+        let text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+    const handleExtraSpaces=()=>{   
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }   
     return (
-        <div>
+        <>
+        <div className='container'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <label htmlFor="myBox" className="form-label"></label>
                 <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange} ></textarea>
                 <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleColorChange}>Change Color</button>
+                <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
+                <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
+                <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
             </div>
+            <div className="container my-3">
+                <h1>Your Text Summary</h1>
+                <p>{text.split(" ").length} words and {text.length} characters  </p>
+                <p>{0.008 * text.split(" ").length} minutes to read</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+                </div>
+
         </div>
+        </>
     )
 }
