d4a63c0f20b116e6a203ba4b1398d25a29c78173
krishnapunjabi708 <krishnapunjabi708@gmail.com>
2026-01-16 19:13:03 +0530

    Alert Added

--- src/App.js
+++ src/App.js
@@ -3,25 +3,40 @@ import './App.css';
 import TextForm from './components/TextForm';
 import Navbar from './components/Navbar';
 import React, { useState } from 'react';
+import Alert from './components/Alert';
+
 // import About from './components/About';
 
 
 function App() {
   let [mode,setMode]=useState('light');//Whether dark mode is enabled or not
+  const[alert,setAlert]=useState(null);
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+  };
+  setTimeout(() => {
+    setAlert(null);
+  }, 3000);
 let toggleMode = () => {
   if (mode === 'light') {
     setMode('dark');
     document.body.style.backgroundColor = '#161c22';
+    showAlert("Dark mode has been enabled","success");
   } else {
     setMode('light');
     document.body.style.backgroundColor = 'white';
+    showAlert("Light mode has been enabled","success");
   }
 }
 
   return (
     <>
 <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} about="About us"/>
-<div className="container"><TextForm mode={mode} heading="Enter text to analyze below"/></div>
+<Alert alert={alert}/>
+<div className="container"><TextForm mode={mode} showAlert={showAlert} heading="Enter text to analyze below"/></div>
 {/* <About /> */}
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,10 +7,12 @@ export default function TextForm(props) {
     const handleUpClick = () => {
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert("Converted to Uppercase","success");
     }
     const handleLoClick = () => {
         let newText = text.toLowerCase();
         setText(newText);
+        props.showAlert("Converted to Lowercase","success");
     }
     const handleOnChange=(event)=>{
         // console.log("On change");
@@ -19,20 +21,24 @@ export default function TextForm(props) {
     const handleClearClick=()=>{     
         let newText = '';
         setText(newText);
+        props.showAlert("Text Cleared","success");
     }
     const handleColorChange=()=>{   
         let newText = text;
         document.getElementById("myBox").style.color="blue";
         setText(newText);
+        props.showAlert("Text Color Changed to Blue","success");
     }
     const handleCopy=()=>{   
         let text = document.getElementById("myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
+        props.showAlert("Text Copied to Clipboard","success");
     }
     const handleExtraSpaces=()=>{   
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "));
+        props.showAlert("Extra Spaces Removed","success");
     }   
     return (
         <>
